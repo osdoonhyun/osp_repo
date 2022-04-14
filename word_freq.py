d7271ea95d9646e5f53e202f5993798e1ad4a20f
osdoonhyun <skw1996@naver.com>
2022-04-14 23:43:16 +0900

    for loop added

--- word_freq.py
+++ word_freq.py
@@ -8,6 +8,18 @@ def CountFrequency(filename, n):
   f=open(filename)
   lines=f.readlines()
 
+ for l in lines:
+    print(l)
+
+  for line in lines:
+    new_string=re.sub(r"[^a-zA-Z0-9]"," ",line)
+
+    print("1:" + new_string)
+    new_list=new_string.split()
+    print(new_list)
+
+    new_dic = dict.fromkeys(new_list)
+
 
 if __name__ == "__main__":
 
