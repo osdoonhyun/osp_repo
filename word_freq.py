c1f2f1ed425a839e515e2d04b8ab67076f22fbc7
osdoonhyun <skw1996@naver.com>
2022-04-14 23:49:47 +0900

    count print

--- word_freq.py
+++ word_freq.py
@@ -8,21 +8,14 @@ def CountFrequency(filename, n):
     f=open(filename)    
     lines=f.readlines()
 
-    for l in lines:
-        print(l)
-
     for line in lines:
         new_string=re.sub(r"[^a-zA-Z0-9]"," ",line)
 
-        print("1:" + new_string)
         new_list=new_string.split()
-        print(new_list)
 
         new_dic = dict.fromkeys(new_list)
 
         text={}
-
-        print(new_dic)
 
         for item in new_dic:
             if (item in text):
