b81280338485232b57b52348f25a5efb2155ac72
osdsoonhyun <87527736+osdoonhyun@users.noreply.github.com>
2022-04-15 19:39:09 +0900

    Hw05 branch (#6)
    * new_file added
    
    * CountFrequency_function added
    
    * main added
    
    * for loop added
    
    * word count added
    
    * word count dic created
    
    * count print
    
    * testing  word_freq.py
    
    * testing word_freq.py
    
    * final modified

--- word_freq.py
+++ word_freq.py
@@ -8,10 +8,28 @@ def CountFrequency(filename, n):
     f=open(filename)    
     lines=f.readlines()
 
+    text= {}
+
     for line in lines:
         new_string=re.sub(r"[^a-zA-Z0-9]"," ",line)
 
         new_list=new_string.split()
+
+        for word in new_list:
+            if (word in text):
+                text[word] += 1
+            else:
+                text[word] = 1
+
+    sortingCntText = sorted(text.items(), key= lambda x: x[1], reverse=True)
+
+    cnt = 0 
+    for key in sortingCntText:
+        cnt = cnt + 1
+        if(cnt <= n):
+            print("%-20s %20d" %(key[0],key[1]))
+        else:
+            break
 
         new_dic = dict.fromkeys(new_list)
 
